c962c2d44b266c4eb6a0055e72832530141dc0f0
Keshav Khandelwal <keshav.csulb@gmail.com>
2017-01-08 13:12:44 -0800

    Node chat app

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-var {generateMessage, generateLocationMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -15,9 +16,23 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {  //Listen for events on server (only for connection or to emit to all we use io else we use socket)
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));    //This will emit to only the client himself
+  socket.on('join', (params, callback) => {
+    if(!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and Room name are required');
+    }
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined')); //emit to all clients except the sender
+    socket.join(params.room);
+    // socket.leave('The Office Fans');
+
+    //io.emit -> io.to(roomName).emit
+    //socket.broadcast.emit -> socket.broadcast.to(roomName).emit
+    //socket.emit
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));    //This will emit to only the client himself
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`)); //emit to all clients except the sender
+
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) => {   //emit for emitter and on for listener; so this will listen from client
     console.log('createMessage', message);
